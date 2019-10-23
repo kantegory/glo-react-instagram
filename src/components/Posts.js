a16c8039efdb7c9f38b1029e40fdc7fe1403662d
kantegory <davidobryakov@gmail.com>
2019-10-23 23:18:46 +0300

    Add JSON-server and some Fetch API

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,76 @@
 import React, {Component} from 'react';
-import Post from './Post';
+import InstaService from '../services/instaService';
+import User from './User';
+import ErrorMessage from './Error';
+
 
 export default class Posts extends Component {
+	InstaService = new InstaService();
+	state = {
+		posts: [],
+		error: false
+	}
+
+	componentDidMount() {
+		this.updatePosts();
+	}
+
+	updatePosts() {
+		this.InstaService.getAllPosts()
+		.then(this.onPostLoaded)
+		.catch(this.onError);
+	}
+
+	onPostLoaded = (posts) => {
+		this.setState({
+			posts,
+			error: false
+		})
+	}
+
+	renderPosts(arr) {
+		return arr.map(post => {
+			const {name, altname, photo, src, alt, descr, id} = post;
+
+			return (
+				<div key={id} className="post">
+					<User 
+						src={photo} 
+						alt={altname}
+						name={name}
+						min
+					/>
+					<img src={src} alt={alt}></img>
+					<div className="post__name">
+						{name}
+					</div>
+					<div className="post__descr">
+						{descr}
+					</div>
+				</div>
+			)
+		})
+	}
+
+	onError = (err) => {
+		console.log('Oh this is probably error', err);
+
+		this.setState({
+			error: true
+		})
+	}
+
 	render() {
+		const {posts, error} = this.state;
+		
+		if (error) {
+			return <ErrorMessage/>
+		}
+
+		const renderedPosts = this.renderPosts(posts);
 		return (
 			<div className="left">
-				<Post src="https://avatars1.githubusercontent.com/u/32088636?s=460&v=4" alt="Triangle" />
-				<Post src="https://sun9-2.userapi.com/c854016/v854016938/268b5/Tehvq1MtBrg.jpg" alt="MyPhoto" />
+				{renderedPosts}
 			</div>
 		)
 	}
